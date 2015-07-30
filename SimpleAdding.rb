72da08dd305415a40a2bbeccf62d1977fc5e02af
fanacea <1117jun@korea.ac.kr>
2015-07-30 23:37:57 +0900

    Create SimpleAdding.rb

---
+++ SimpleAdding.rb
@@ -0,0 +1,14 @@
+def SimpleAdding(num)
+
+  # code goes here
+  counter = 0
+  sum = 0
+  while counter < num
+    sum += (counter+1)
+    counter += 1
+  end
+  
+  return sum 
+         
+end
+   
